835cd4f40964525f1dab1ab05f72d9aa2127afd4
AdiIrawan21 <adiirawan01012003@gmail.com>
2024-05-03 13:11:06 +0700

    feat: updated endpoint/

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 const mediaRoutes = require('./routes/media.routes');
 app.use('/api/v1', mediaRoutes);
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello, ini challenge chapter 6, silakan akses /api-docs untuk melihat documentation api");
 })
 
