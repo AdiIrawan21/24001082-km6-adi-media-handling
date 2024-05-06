90b73c80903a00da9785798f03b20169c286dba7
AdiIrawan21 <adiirawan01012003@gmail.com>
2024-05-06 20:43:43 +0700

    feat: add sentry.io in app.js

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const Sentry = require("@sentry/node")
+
+// Handle sentry.io untuk logging di production
+const { SENTRY_DSN, ENV } = process.env
+Sentry.init({
+    environment: ENV,
+    dsn: SENTRY_DSN,
+    integrations: []
+})
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+// End handle sentry.io
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Swagger
 const swaggerUi = require("swagger-ui-express");
@@ -23,10 +37,20 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 const mediaRoutes = require('./routes/media.routes');
 app.use('/api/v1', mediaRoutes);
 
+// app.get('/', (req, res) => {
+//     res.send("Hello, ini challenge chapter 6, silakan akses /api-docs untuk melihat documentation api");
+// })
+
 app.get('/', (req, res) => {
-    res.send("Hello, ini challenge chapter 6, silakan akses /api-docs untuk melihat documentation api");
+    res.json({
+        status: true,
+        message: "Hello, ini challenge chapter 6, silakan akses /api-docs untuk melihat documentation api",
+        data: null
+    })
 })
 
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // 500 error handler
 app.use((err, req, res, next) => {
